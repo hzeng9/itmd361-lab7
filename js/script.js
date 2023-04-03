ba8a56980849b62fb12c0648092ae95d6451fe14
hzeng9 <hzeng9@hawk.iit.edu>
2023-04-03 12:37:41 -0500

    modified script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
+  let button = document.getElementById('entrybutton');
+
+  function alertButton(){
+    let text = document.getElementById("entryinput").value;
+    document.getElementById('textoutput').innerText = text;
+    alert("Hao Yuan Zeng: " + text);
+  }
+
+  button.addEventListener('click', alertButton);
  
-
-
-
-
-
-
+}
 
 window.addEventListener('load', init);
